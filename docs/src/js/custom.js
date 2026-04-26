81e9fd4c2e9a36b41cb95a2b518fcdfd77729609
smk <sudhanshumohan781@gmail.com>
2026-04-26 11:30:13 +0530

    fix matjax issue

--- docs/src/js/custom.js
+++ docs/src/js/custom.js
@@ -1,10 +1,17 @@
-document$.subscribe(function () {
+function renderMath() {
   renderMathInElement(document.body, {
     delimiters: [
       { left: "$$", right: "$$", display: true },
-      { left: "$", right: "$", display: false },
+      { left: "$",  right: "$",  display: false },
       { left: "\\(", right: "\\)", display: false },
       { left: "\\[", right: "\\]", display: true },
     ],
+    throwOnError: false,
   });
-});
+}
+
+if (typeof document$ !== "undefined") {
+  document$.subscribe(renderMath);
+} else {
+  document.addEventListener("DOMContentLoaded", renderMath);
+}
